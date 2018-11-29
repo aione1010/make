738b292d49bc9aa0c3bbca7d4ac5ead8f69a04bd
make <860817705@qq.com>
2018-11-29 16:17:31 +0800

    Update sort_insertionSort.py

--- python/python-algorithm/sort/sort_insertionSort.py
+++ python/python-algorithm/sort/sort_insertionSort.py
@@ -1,17 +1,18 @@
 def insertionSort(alist):
+    
+    #长度为0或者为1
+   if len(alist)<=1:
+        return alist
+    else:
+        for i in range(2,len(alist)):
+            currentValue = alist[i]
+            pos = i
+            while pos > 0 and alist[pos-1] > currentValue:   #在pos之前的值与currentValue比较，只要比currentValue大就后移，空出一位来
+                alist[pos] = alist[pos-1]
+                pos = pos-1
+            alist[pos] = currentValue
+        return alist
 
-    for i in range(1,len(alist)):
-
-        currentValue = alist[i]
-        position = i
-
-        while position > 0 and alist[position-1] > currentValue:
-            alist[position] = alist[position -1]
-            position = position - 1
-
-        alist[position] = currentValue
-
-    return alist
-
+    
 aList=[54,56,85,91,20,3,26,59,22,30,20,20,20]
-print(insertionSort(aList))
+print(insertSort(aList))
