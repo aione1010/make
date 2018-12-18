421aa3a5b754d5835d9ee5e83ed746b3db9b52c6
aione110 <860817705@qq.com>
2018-12-18 22:11:06 +0800

    排列

--- leetcode/letterCombinations.js
+++ leetcode/letterCombinations.js
@@ -1,3 +1,5 @@
+
+//方法一
 var f1=function(str1,str2){
     var arr = []
     for(var i=0;i<str1.length;i++){
@@ -22,4 +24,30 @@ var letterCombinations = function(digits) {
     }else{
         return f1(dic[parseInt(digits[0])-2],letterCombinations(digits.slice(1)))
     }
+};
+
+//方法二
+let letterCombinations = function(digits) {
+    if (digits === '') return [];
+    let dict = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+    let res = [''];
+    for (let digit of digits) {
+        let len = res.length;
+        while (len-- > 0) {
+            let e = res.shift();
+            for (let c of dict[digit]) {
+                res.push(e + c);
+            }
+        }
+    }
+    return res;
 };
