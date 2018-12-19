34478f1c68bc3ea3ad5fdab5b25150bf3c001bae
make <860817705@qq.com>
2018-12-19 18:18:44 +0800

    Update getLongestPalindrome.py

--- python/python-algorithm/dynamic programming/getLongestPalindrome.py
+++ python/python-algorithm/dynamic programming/getLongestPalindrome.py
@@ -16,7 +16,7 @@ def getLongestPalindrome(s):
             #的子字符串是否为回文，如果是，更新
             for i in range(n):
                 if i-maxLen >= 1 and s[i-maxLen - 1:i + 1] == s[i-maxLen - 1:i+1][::-1]:
-                    s_p = s[i-maxLen - 1:i + 1]
+                    s_p = s[i-maxLen - 1:i + 1]   #该处取索引，不取结果的话，速度会提升；
                     maxLen += 2     #回文关于中间对称，例如“abcba”
                     continue
                 if i - maxLen >= 0 and s[i-maxLen:i + 1] == s[i-maxLen:i+1][::-1]:
@@ -25,3 +25,21 @@ def getLongestPalindrome(s):
             return maxLen,s_p
     
 print(getLongestPalindrome("abc1234321ab"))
+
+"""
+def f(s):
+    n = len(s)
+    if n < 2 or s==s[::-1]:
+        return s
+    else:
+        maxLen=1
+        start = 0
+        for i in range(1,n):
+            if i-maxLen >= 0 and s[i-maxLen:i+1] == s[i-maxLen:i+1][::-1]:
+                start = i-maxLen    #只取索引，值在最后取
+                maxLen +=1
+            if i-maxLen-1 >= 0 and s[i-maxLen-1:i+1] == s[i-maxLen-1:i+1][::-1]:
+                start = i-maxLen-1   #只取索引，值在最后取
+                maxLen +=2
+        return s[start:start+maxLen]   #此处取值
+"""
