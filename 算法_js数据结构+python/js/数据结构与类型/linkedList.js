0c232b426ff78536fd843e19a25196268648d9c7
aione110 <860817705@qq.com>
2018-12-24 17:40:31 +0800

    '链表'

--- 算法_js数据结构+python/js/数据结构与类型/linkedList.js
+++ 算法_js数据结构+python/js/数据结构与类型/linkedList.js
@@ -3,4 +3,42 @@ function LinkedList(){
         this.element = element;
         this.next = null;
     }
+    let length = 0;
+    let head = null;
+    //添加元素到链表末尾
+    this.append = function(element){
+        let node = new Node(element),
+            current;
+        if(head==null){
+            head = node;
+        }else{
+            current = head;
+            while(current.next){
+                current = current.next; 
+            }
+            current.next = node;
+        }
+        length ++;
+    }
+    this.removeAt = function(pos){
+        if(pos > -1 && pos < length){
+            let current = head,
+                previous,
+                index = 0;
+            if (pos ===0){
+                head = current.next;
+            }else{
+                while(index ++ < position){
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            length --;
+            return current.element;
+        }
+        else{
+            return null;
+        }
+    }
 }
