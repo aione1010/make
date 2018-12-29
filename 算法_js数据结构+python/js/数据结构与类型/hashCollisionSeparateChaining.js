ddc99433e7e7ec05ad06ce9e0e165886820e697e
aione110 <m20101083@163.com>
2018-12-30 01:37:41 +0800

    hashtable

---
+++ 算法_js数据结构+python/js/数据结构与类型/hashCollisionSeparateChaining.js
@@ -0,0 +1,33 @@
+function HashTableSeparateChaining(){
+    var table = [];
+
+    var ValuePair = function(key,value){
+        this.key = key;
+        this.value = value;
+        this.toString = function(){
+            return '[' + this.key + '-' + this.value + ']';
+        }
+    };
+
+    var loseloseHashCode = function(key){
+        var hash = 0;
+        for (var i = 0; i < key.length; i++){
+            hash += key.charCodeAt(i);
+        }
+        return hash % 37;
+    };
+
+    this.put = function(key,value){
+        var pos = loseloseHashCode(key);
+        if(table[pos] === undefined){
+            table[pos] = new LinkedList();
+        }
+        table[pos].append(new ValuePair(key,value));
+    };
+
+    this.get = function(key){
+        var pos = loseloseHashCode(key);
+
+        if
+    }
+}
--- 算法_js数据结构+python/js/数据结构与类型/hashTable.js
+++ 算法_js数据结构+python/js/数据结构与类型/hashTable.js
@@ -1,11 +1,34 @@
 function HashTable(){
     var table = [];
     
-    var loseloseHashCode = function(key){
+    var loseloseHashCode = function(key){       //把key的每个字母的ASCII码相加，获得的和就是hash
         var hash = 0;
         for (var i = 0; i < key.length; i ++){
             hash += key.charCodeAt(i);
         }
-        return hash % 37;
-    }
+        return hash % 37;                     //为了得到一个比较小的数，可以把hash值和任意数相除得余数。
+    };
+
+    this.put = function(key,value){
+        var pos = loseloseHashCode(key);
+        //console.log(position + ' - ' + key);
+        table[pos] = value;
+    };
+
+    this.get = function(key){
+        return table[loseloseHashCode(key)];
+    };
+
+    this.remove = function(key){
+        table[loseloseHashCode(key)] = undefined;
+    };
+
+    this.print = function(){
+        for(var i = 0;i < table.length; i++){
+            if(!table[i]==undefined){
+                console.log(i + ':' + table[i]);
+            }
+        }
+    };
+
 }
